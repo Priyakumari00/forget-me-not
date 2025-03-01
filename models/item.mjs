c6981da7081555e51500b9c57b06974bb5abe430
Priya Kumari <priya.k.p.13579@gmail.com>
2025-03-01 19:38:45 +0530

    modelcomplete

---
+++ models/item.mjs
@@ -0,0 +1,25 @@
+
+import mongoose from "mongoose";
+
+
+const ItemSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    user :{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+
+        
+    },
+
+    store : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Store",
+        required:true,
+    },
+})
+
+export const ItemModel = mongoose.model("Item",ItemSchema);
---
+++ models/store.mjs
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const StoreSchema = new mongoose.Schema ({
+    name:{
+        type:String,
+        required: true
+    }
+})
+
+export const StoreModel =mongoose.model("Store",StoreSchema);
