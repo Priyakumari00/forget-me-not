cdc26db28bdbd14064e4ee9efcb2561ae9acacc9
Priya Kumari <priya.k.p.13579@gmail.com>
2025-03-01 16:42:01 +0530

    model

---
+++ models/user.mjs
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    phone: {
+        type: String
+    },
+})
+export const UserModel = mongoose.model("User", UserSchema);
