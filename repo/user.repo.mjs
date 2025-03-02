fddf70e164421316b770bc68eaaf16482f62eb9d
Priya Kumari <priya.k.p.13579@gmail.com>
2025-03-02 16:17:43 +0530

    repostore

--- repo/user.repo.mjs
+++ repo/user.repo.mjs
@@ -1,20 +1,20 @@
 import { UserModel } from "../models/user.mjs";
 
-export class UserRepo {
-    static async findByEmail(email) {
-        const result = await UserModel.findOne({ email: email })
-        if (result == undefined || result == null) {
+export class UserRepo{
+    static async findByEmail(email){
+        const result = await UserModel.findOne({email:email})
+        if(result == undefined || result == null){
             return null;
         }
         return result;
     }
 
-    static async createUser(name, email, password, phone) {
-        const user = await UserModel.create({ name: name, email: email, password: password, phone: phone })
-        if (user == undefined || user == null) {
+    static async createUser(name, email, password, phone){
+        const user = await UserModel.create({name:name,email:email,password:password, phone: phone})
+        if(user == undefined || user == null){
             throw new Error("Unable to create user");
         }
-        return user;
+       return user;
     }
 }
 
